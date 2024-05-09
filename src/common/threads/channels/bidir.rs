bd336994ce741ad4cb4d74f94c1011694d5f7bd8
Jack Summers <jsumme205@gmail.com>
2024-05-08 17:49:19 -0700

    Another Commit

--- src/common/threads/channels/bidir.rs
+++ src/common/threads/channels/bidir.rs
@@ -2,5 +2,7 @@ use std::{collections::VecDeque, sync::{atomic::AtomicUsize, Arc, RwLock}};
 
 struct RawShared<T, U> {
     queue_one: Arc<RwLock<VecDeque<T>>>,
-    queue_two: Arc<RwLock<VecDeque<T>>>
+    queue_two: Arc<RwLock<VecDeque<U>>>
 }
+
+enum 
